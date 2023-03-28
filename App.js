72d254a6c42b6294c655bbacba4c5dac5645d07d
Minh Tran <mtran1712@gmail.com>
2023-03-27 21:42:30 -0700

    feat: cleaned up main page

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import AppContainer from './src/components/app-container'
 import MainScreen from './src/screens/main'
 
