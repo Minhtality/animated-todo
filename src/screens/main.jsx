4b0c1e01b45d8bba6427448348b446a18c8f6e48
Minh Tran <mtran1712@gmail.com>
2023-03-29 14:00:15 -0700

    feat: added animated checkbox

--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import { StyleSheet, View, Pressable } from 'react-native'
 import {
   Text,
   Box,
@@ -10,8 +11,16 @@ import {
   useColorModeValue
 } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from 'react-native-checkbox-reanimated'
 
 export default function MainScreen() {
+  const [checked, setChecked] = useState(false)
+
+  const handleCheckboxPress = () => {
+    setChecked(prev => {
+      return !prev
+    })
+  }
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -20,11 +29,31 @@ export default function MainScreen() {
       flex={1}
     >
       <VStack space={5} alignItems="center">
-        <Box>
-          <Text fontWeight="bold">Welcome to React Native</Text>
-          <ThemeToggle />
+        <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+          <Text fontWeight="bold">Hello</Text>
         </Box>
+        <ThemeToggle />
+        <Pressable onPress={handleCheckboxPress} style={styles.checkbox}>
+          <AnimatedCheckbox
+            checked={checked}
+            highlightColor="#4444ff"
+            checkmarkColor="#ffffff"
+            boxOutlineColor="#4444ff"
+          />
+        </Pressable>
       </VStack>
     </Center>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  checkbox: {
+    width: 64,
+    height: 64
+  }
+})
